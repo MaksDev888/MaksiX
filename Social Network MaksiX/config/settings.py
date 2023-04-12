27fde5ef98f9eb95c179c306797e53012771d077
Maks <maks.shersnv@inbox.ru>
2023-04-12 18:48:12 +0300

    v1.22

--- Social Network MaksiX/config/settings.py
+++ Social Network MaksiX/config/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "ENGINE": "django.db.backends.postgresql",
         "NAME": config("DB_NAME"),
         "USER": config("DB_USER"),
         "PASSWORD": config("DB_PASSWORD"),
